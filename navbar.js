c75d0d52310aa3eee4f78facf28184aa6d0e85eb
darkksuns <kyla.orchard@gmail.com>
2024-04-24 17:04:12 +0800

    added closing on esc key press/clicking outside the window, dimming the background when overlay is open/making it uninteractable and changed placeholder text

--- navbar.js
+++ navbar.js
@@ -1,13 +1,17 @@
 // Function to open the about pop-up box from the navbar
 function displayAboutPage() {
   const aboutBox = document.querySelector(".about-pop-up");
+  const overlay = document.querySelector(".overlay");
   aboutBox.classList.add("display-pop-up");
+  overlay.style.display = "block";
 }
 
 // Function to close about pop-up box
 function closeAboutPage() {
   const aboutBox = document.querySelector(".about-pop-up");
+  const overlay = document.querySelector(".overlay");
   aboutBox.classList.remove("display-pop-up");
+  overlay.style.display = "none";
 }
 
 // Event listener for about button
@@ -17,4 +21,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const closeButton = document.querySelector(".about-pop-up button");
   closeButton.addEventListener("click", closeAboutPage);
+
+  // Close overlay when clicking on the outside of the box
+  const overlay = document.querySelector(".overlay");
+  overlay.addEventListener("click", closeAboutPage);
+
+  // Close overlay when pressing Esc key
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      closeAboutPage();
+    }
+  });
 });
