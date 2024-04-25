ee08e1623c52647ace4ffe4023ff418be0233339
DJRyLau <ryan-lau@hotmail.com>
2024-04-25 20:51:04 +0800

    Smooth Out Overlay Dimming
    Implemented easing opacity transition

--- navbar.js
+++ navbar.js
@@ -2,8 +2,11 @@
 function displayAboutPage() {
   const aboutBox = document.querySelector(".about-pop-up");
   const overlay = document.querySelector(".overlay");
-  aboutBox.classList.add("display-pop-up");
   overlay.style.display = "block";
+  setTimeout(() => { // This timeout ensures the display is set before starting opacity transition
+    overlay.style.opacity = "1";
+    aboutBox.classList.add("display-pop-up");
+  }, 10); // Short delay to ensure the display change has taken effect
 }
 
 // Function to close about pop-up box
@@ -11,7 +14,10 @@ function closeAboutPage() {
   const aboutBox = document.querySelector(".about-pop-up");
   const overlay = document.querySelector(".overlay");
   aboutBox.classList.remove("display-pop-up");
-  overlay.style.display = "none";
+  overlay.style.opacity = "0";
+  setTimeout(() => { // Delay to wait for the opacity transition to finish before hiding the element
+    overlay.style.display = "none";
+  }, 300); // This should match the duration of the opacity transition
 }
 
 // Event listener for about button
