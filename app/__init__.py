4aae306bd9016c83fa6038e8c72fd0231451ae26
ciaranengelbrecht <cizzaengel@gmail.com>
2024-05-15 22:06:40 +0800

    Refactor the entire repo to reflect tutorial implementation of testing and using correct file separation for routes, init and config

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+# app/__init__.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
+from .config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+csrf = CSRFProtect()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+    csrf.init_app(app)
+
+    login_manager.login_view = 'app.authentication'
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from .routes import app as app_blueprint
+    app.register_blueprint(app_blueprint)
+
+    return app
