1e05be8fba60e92df2e11de644edfd2343360591
ciaranengelbrecht <cizzaengel@gmail.com>
2024-05-19 14:51:58 +0800

    Fixed testing config

--- app/config.py
+++ app/config.py
@@ -8,3 +8,8 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    WTF_CSRF_ENABLED = False 
